5d2a9c12cdad27070732ad10c87e31b882ed1f5a
Jason Beckmann <jasonabeckmann@gmail.com>
2025-10-10 19:08:31 -0400

    fixed avante config

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -5,20 +5,22 @@ return {
 	opts = {
 		provider = "openai",
 		auto_suggestions_provider = "openai",
-		openai = {
-			endpoint = "https://api.deepseek.com/v1",
-			model = "deepseek-chat",
-			max_tokens = 4096,
+		providers = {
+			openai = {
+				endpoint = "https://api.deepseek.com/v1",
+				model = "deepseek-chat",
+				max_tokens = 4096,
+			}
 		}
-	-- add any opts here
+		-- add any opts here
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
 	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 	dependencies = {
-		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		"hrsh7th/nvim-cmp",
+		"stevearc/dressing.nvim",
 	}
 }
